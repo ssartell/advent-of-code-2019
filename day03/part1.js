94c1e154395f19f18af772394d039ee844c8bf79
Sean Sartell <ssartell@rightpoint.com>
2019-12-03 11:12:19 -0600

    update

--- day03/part1.js
+++ day03/part1.js
@@ -1,8 +1,47 @@
 const R = require('ramda');
-const debug = x => { debugger; return x; };
 
-const parseInput = R.pipe(R.trim);
+const stepRegex = /(\w)(\d+)/;
+const parseStep = R.pipe(R.match(stepRegex), R.tail, R.zipObj(['dir', 'length']), R.evolve({ 'length': parseInt }));
+const parseLine = R.pipe(R.split(','), R.map(parseStep));
+const parseInput = R.pipe(R.trim, R.split('\n'), R.map(parseLine));
 
-const solution = R.pipe(parseInput, debug);
+const dirs = {
+    U: { x: 0, y: -1 },
+    D: { x: 0, y: 1 },
+    L: { x: -1, y: 0 },
+    R: { x: 1, y: 0 }
+};
+
+const extendWire = wireSteps => {
+    let wire = new Map();
+    let pos = { x: 0, y: 0 };
+    
+    for(let step of wireSteps) {
+        let dir = dirs[step.dir];
+        for(let i = 0; i < step.length; i++) {
+            pos.x += dir.x;
+            pos.y += dir.y;
+            wire.set(`${pos.x},${pos.y}`, Math.abs(pos.x) + Math.abs(pos.y));
+        }
+    }
+
+    return wire;
+}
+
+const nearestIntersection = wires => {
+    let [wire1, wire2] = wires;
+    let shortestDist = Infinity;
+
+    for(let pos of wire1.keys()) {
+        if (wire2.has(pos)) {
+            let dist = wire1.get(pos);
+            shortestDist = Math.min(shortestDist, dist);
+        }
+    }
+
+    return shortestDist;
+}
+
+const solution = R.pipe(parseInput, R.map(extendWire), nearestIntersection);
 
 module.exports = solution;
