fb9566e897ed1667c9b11c56cf88480cfa4a3cac
Sean Sartell <ssartell@rightpoint.com>
2019-12-04 08:03:00 -0600

    update

---
+++ day04/part1.js
@@ -0,0 +1,12 @@
+const R = require('ramda');
+
+const parseInput = R.pipe(R.trim, R.split('-'), R.map(parseInt));
+
+const toString = x => `${x}`;
+const eachDigit = R.pipe(toString, R.split(''), R.map(parseInt));
+const neverDecreases = R.pipe(eachDigit, R.groupWith(R.lte), R.length, R.equals(1));
+const twoAdjacentEqual = R.pipe(eachDigit, R.groupWith(R.equals), x => x.length < 6);
+
+const solution = R.pipe(parseInput, R.apply(R.range), R.filter(neverDecreases), R.filter(twoAdjacentEqual), R.length);
+
+module.exports = solution;
