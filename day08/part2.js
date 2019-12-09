4f63aa11bb880b03da6dca088eb065141498371a
Sean Sartell <ssartell@rightpoint.com>
2019-12-08 22:07:01 -0600

    update

--- day08/part2.js
+++ day08/part2.js
@@ -3,7 +3,7 @@ const [w, h] = [25, 6];
 
 const parseInput = R.pipe(R.trim, R.split(''), R.map(parseInt), R.splitEvery(w * h));
 const applyPixel = (a, b) => b < 2 ? b : a;
-const print = R.pipe(R.splitEvery(w), R.map(R.join('')), R.join('\n'), R.replace(/0/g, ' '), R.replace(/1/g, 'X'));
+const print = R.pipe(R.join(''), R.splitEvery(w), R.join('\n'), R.replace(/0/g, ' '), R.replace(/1/g, 'X'));
 
 const solution = R.pipe(parseInput, R.reverse, R.reduce(R.zipWith(applyPixel), new Array(w * h)), print);
 
