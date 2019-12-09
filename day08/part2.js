a2a20f72f98186bf9d78ef5b659b9842fef0b805
Sean Sartell <ssartell@rightpoint.com>
2019-12-08 22:03:32 -0600

    update

--- day08/part2.js
+++ day08/part2.js
@@ -1,10 +1,10 @@
 const R = require('ramda');
-const [width, height] = [25, 6];
+const [w, h] = [25, 6];
 
-const parseInput = R.pipe(R.trim, R.split(''), R.map(parseInt), R.splitEvery(width * height));
+const parseInput = R.pipe(R.trim, R.split(''), R.map(parseInt), R.splitEvery(w * h));
 const applyPixel = (a, b) => b < 2 ? b : a;
-const print = R.pipe(R.splitEvery(width), R.map(R.join('')), R.join('\n'), R.replace(/0/g, ' '), R.replace(/1/g, 'X'));
+const print = R.pipe(R.splitEvery(w), R.map(R.join('')), R.join('\n'), R.replace(/0/g, ' '), R.replace(/1/g, 'X'));
 
-const solution = R.pipe(parseInput, R.reverse, R.reduce(R.zipWith(applyPixel), R.repeat(2, width * height)), print);
+const solution = R.pipe(parseInput, R.reverse, R.reduce(R.zipWith(applyPixel), new Array(w * h)), print);
 
 module.exports = solution;
